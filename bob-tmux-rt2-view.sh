ad8f04d2e2a52cfe1670f496a6a52f98faa1f964
Jonathan Guo <jonathan.guo@lynqnetworks.com>
2025-09-18 13:06:38 -0400

    updated rt2 view for configure app, unkindness app, and new debug ports

--- bob-tmux-rt2-view.sh
+++ bob-tmux-rt2-view.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
 
-# Helper function to launch a horizontal split with history tracking
-run_with_history() {
-    local percent="$1"
-    local cmd="$2"
-    tmux split-window -h -l "$percent" \
-        "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"$cmd\"; $cmd')"
-}
+use_unkindness=false
 
-# Helper function to launch a vertical split with history tracking
-run_vert_with_history() {
-    local percent="$1"
-    local cmd="$2"
-    tmux split-window -v -f -l "$percent%" \
-        "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"$cmd\"; $cmd')"
-}
+# Parse options
+while getopts ":u" opt; do
+  case $opt in
+    u) use_unkindness=true ;;
+    \?) echo "Usage: $0 [-u] device_indices..."; exit 1 ;;
+  esac
+done
+shift $((OPTIND - 1))
 
 num_devices=$#
 device_indices=("$@")
 
-# --- Start tmux session for first device ---
-
 # Create a new window for the first host console
 tmux new-window -n "BoBs" \
-    "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"minicom -D /dev/rtHST${device_indices[0]}-0\"; minicom -D /dev/rtHST${device_indices[0]}-0')"
+    "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"minicom -b 921600 -D /dev/rtDBG${device_indices[0]}-1\"; minicom -b 921600 -D /dev/rtDBG${device_indices[0]}-1')"
 
-# Add DBG and SDK panes horizontally
-run_with_history 75% "minicom -b 921600 -D /dev/rtDBG${device_indices[0]}-1"
-run_with_history 66% "minicom -D /dev/rtDBG${device_indices[0]}-0"
-run_with_history 50% "cd \"$HOME/dev/raven-sdk\" && ./bin/configure.debug /dev/rtHST${device_indices[0]}-1"
-
-# --- Add additional devices vertically ---
+# Loop through the rest of the devices and attach debug for 9151
 for i in $(seq 1 $((num_devices - 1))); do
-    remaining=$((num_devices - i + 1))
-    percent=$((100 / remaining))
-
-    tmux select-pane -t 0
-    run_vert_with_history "$percent" "minicom -D /dev/rtHST${device_indices[$i]}-0"
-
-    run_with_history 75% "minicom -b 921600 -D /dev/rtDBG${device_indices[$i]}-1"
-    run_with_history 66% "minicom -D /dev/rtDBG${device_indices[$i]}-0"
-    run_with_history 50% "cd \"$HOME/dev/raven-sdk\" && ./bin/configure.debug /dev/rtHST${device_indices[$i]}-1"
+    tmux split-window -h -c "#{pane_current_path}" \
+    "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"minicom -b 921600 -D /dev/rtDBG${device_indices[$i]}-1\"; minicom -b 921600 -D /dev/rtDBG${device_indices[$i]}-1')"
 done
+
+
+tmux select-layout even-horizontal
+tmux select-pane -t 1
+
+#cd to unkindness test
+if $use_unkindness; then
+    for ((i=0; i<num_devices; i++)); do
+    
+        tmux split-window -c "#{pane_current_path}" \
+        "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"cd $HOME/dev/unkindess-test && ./bin/devtest --sdk_port /dev/rtDBG${device_indices[$i]}-0 --formation_cycles 100 --formation_cycle_time 15 --formation_time 20\"')"
+    
+        tmux select-pane -R
+    done
+fi
+
+#attach to configure app for each device
+for ((i=0; i<num_devices; i++)); do
+
+    tmux split-window -c "#{pane_current_path}" \
+    "exec bash --rcfile <(echo 'source ~/.bashrc; history -s \"cd $HOME/dev/raven-sdk && ./bin/configure.debug /dev/rtDBG${device_indices[$i]}-0\"; cd $HOME/dev/raven-sdk && ./bin/configure.debug /dev/rtDBG${device_indices[$i]}-0')"
+
+    tmux select-pane -R
+done
+
+
+
 
